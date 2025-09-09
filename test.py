faa6340f029917e1fd9bc7ce3bb2906de3966c7d
martasvobo <62069655+martasvobo@users.noreply.github.com>
2025-09-09 22:30:54 +0100

    nice changes

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 import subprocess
-import time
 
 NUM_CLIENTS = 50
 clients = []
@@ -8,7 +7,6 @@ for i in range(NUM_CLIENTS):
     p = subprocess.Popen(["./NetworkClient"])
     clients.append(p)
 
-time.sleep(20)  # run for 20s
 
 for p in clients:
-    p.terminate()
+    p.wait()
